b610cb5b631fd1865f10886903c3f2a3ae53db89
Ana <ana@foo.com>
2013-12-12 21:48:09 -0500

    add command line arguments

--- test.rb
+++ test.rb
@@ -1,88 +1,68 @@
 require_relative 'game'
 require 'pry'
+require 'optparse'
 
-def setup_1
-  game = Game.new(4,4)
-  [true, false, false, true].each { |x| game.add x }
-  [true, true, false, true].each { |x| game.add x }
-  [false, true, true, true].each { |x| game.add x }
-  game
-end
-
-
-def setup_2
-  game = Game.new(5,5)
-  [true, false, false, true, true ].each { |x| game.add x }
-  [true, true, false, false, true].each { |x| game.add x }
-  [false, true, true, true, true].each { |x| game.add x }
-  game
-end
-
-
-def setup_3
-  game = Game.new(4,4)
-  [false, false, false, false].each { |x| game.add x }
-  [false, true,  true, false].each { |x| game.add x }
-  [false, true,  true, false].each { |x| game.add x }
-  [false, false, false, false].each { |x| game.add x }
-  game
-end
-
-def setup_4
-  game = Game.new(5,5)
-  [false, false, false, false, false].each { |x| game.add x }
-  [false, false, true,  false, false].each { |x| game.add x }
-  [false, false, true,  false, false].each { |x| game.add x }
-  [false, false, true,  false, false].each { |x| game.add x }
-  [false, false, false, false, false].each { |x| game.add x }
-  game
-end
-
-def game_from(str)
-  rows = str.split(/\n/).collect {|r| r.strip.split // }
-  nrows = rows.size
-  ncols = rows.inject(0) { |max, r| max = r.size if max < r.size; max }
-  game = Game.new(nrows, ncols)
-  rows.each do |row|
-    row.each do |cell|
-      game.add cell == '1'
-    end
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: test.rb [options]"
+  opts.on("-g name", "--game name", "which game to run") do |name|
+    options[:game] = name
   end
-  game
+  opts.on("-l", "--list", "list available games") do
+    options[:list] = true
+  end
+  opts.on("-s name", "--show name", "show setup for name") do |name|
+    options[:show] = name
+  end
+  opts.on("-p", "--prompt", "prompt in between iterations") do |arg|
+    options[:prompt] = true
+  end
+  opts.on("-i n", "--iterations n", "number of iterations") do |arg|
+  options[:iterations] = arg.to_i
+  end
+  opts.on("-d secs", "--delay secs", "seconds to delay") do |arg|
+    options[:delay] = arg.to_f
+  end
+  opts.on("-C", "--no-clear", "don't clear the screen in between") do |arg|
+    options[:clear] = false
+  end
+
+end.parse!
+
+@games = {}
+def game(name)
+  @games[name] = yield
 end
 
-# a glider
-def setup_5
+game 'spaceship' do
+  board =<<-END
+  001000
+  000100
+  011100
+  000000
+  000000
+  END
+end
+
+game 'pulsar1' do
 board =<<-END
-001000
-000100
-011100
-000000
-000000
+  0011100011100
+  0000000000000
+  1000010100001
+  1000010100001
+  1000010100001
+  0011100011100
+  0000000000000
+  0011100011100
+  1000010100001
+  1000010100001
+  1000010100001
+  0000000000000
+  0011100011100
 END
 end
 
-# a pulsar
-def setup_6
-board =<<-END
-  0011100011100
-  0000000000000
-  1000010100001
-  1000010100001
-  1000010100001
-  0011100011100
-  0000000000000
-  0011100011100
-  1000010100001
-  1000010100001
-  1000010100001
-  0000000000000
-  0011100011100
-END
-end
-
-# a pulsar
-def setup_7
+game 'pulsar2' do
 board =<<-END
   0010000100
   1101111011
@@ -90,8 +70,7 @@ board =<<-END
 END
 end
 
-# should be a glider (broken)
-def setup_8
+game 'broken_glider' do
 board =<<-END
   0000001
   0000010
@@ -107,8 +86,7 @@ board =<<-END
 END
 end
 
-# acorn
-def setup_acorn
+game 'acorn' do
 board =<<-END
   0100000
   0001000
@@ -116,16 +94,14 @@ board =<<-END
 END
 end
 
-# pulsar (toad)
-def setup_toad
+game 'toad' do
 board =<<-END
   0111
   1110
 END
 end
 
-# pulsar (beacon)
-def setup_beacon
+game 'beacon' do
 board =<<-END
   1100
   1100
@@ -134,7 +110,7 @@ board =<<-END
 END
 end
 
-def setup_diehard
+game 'diehard' do
 board =<<-END
   00000000
   00000010
@@ -143,8 +119,7 @@ board =<<-END
 END
 end
 
-# glider gun
-def setup_glider_gun
+game 'glider_gun' do
 board =<<-END
 0000000000000000000000001000000000000000
 0000000000000000000000101000000000000000
@@ -158,41 +133,71 @@ board =<<-END
 END
 end
 
-clear_code = %x(clear)
-
-#game = game_from(setup_glider_gun)
-#game = game_from(setup_diehard)
-game = game_from(setup_acorn)
-game.dead_char = ' '
-
-prompt = false
-
-5206.times do |i|
-  print clear_code
-#  puts
-
-  game.pprint
-  if game.nliving.zero?
-    puts "nothing left alive #{i}"
-    break
-  end
-  if game.steady_state
-    puts "steady state"
-    break
-  end
-  puts i
-
-  if prompt
-    input = STDIN.gets.strip
-    case input
-      when "b"
-        break
-      when "p"
-        binding.pry
+def game_from(str)
+  rows = str.split(/\n/).collect {|r| r.strip.split // }
+  nrows = rows.size
+  ncols = rows.inject(0) { |max, r| max = r.size if max < r.size; max }
+  game = Game.new(nrows, ncols)
+  rows.each do |row|
+    row.each do |cell|
+      game.add cell == '1'
     end
-  else
-    sleep(0.05)
   end
+  game
+end
 
-  game.tick
+def play(name, options)
+  clear_code = %x(clear)
+
+  game = game_from(@games[name])
+  game.dead_char = ' '
+
+  prompt = options[:prompt] || false
+  iterations = options[:iterations] || 50
+  delay = options[:delay] || 0.5
+  clear = options.has_key?(:clear) ? options[:clear] : true
+
+  iterations.times do |i|
+    clear ? print( clear_code ) : puts
+
+    game.pprint
+    if game.nliving.zero?
+      puts "nothing left alive #{i}"
+      break
+    end
+    if game.steady_state
+      puts "steady state"
+      break
+    end
+    puts i
+
+    if prompt
+      input = STDIN.gets.strip
+      case input
+        when "b"
+          break
+        when "p"
+          binding.pry
+      end
+    else
+      sleep(delay)
+    end
+
+    game.tick
+  end
+end
+
+if options[:list]
+  puts @games.keys.sort
+  exit
+end
+
+if options[:show]
+  name = options[:show]
+  puts @games[name]
+  exit
+end
+
+if name = options[:game]
+  play(name, options)
 end
